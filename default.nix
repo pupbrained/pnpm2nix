86c839b82974eea16d31a10ca2a96477a523cfb1
Mars <mars@pupbrained.xyz>
2022-10-10 19:07:41 -0400

    Update default.nix

--- default.nix
+++ default.nix
@@ -1,6 +1,6 @@
 { pkgs ? import <nixpkgs> {}
-, nodejs ? pkgs.nodejs-8_x
-, nodePackages ? pkgs.nodePackages_8_x
+, nodejs ? pkgs.nodejs-16_x
+, nodePackages ? pkgs.nodePackages_16_x
 , node-gyp ? nodePackages.node-gyp
 } @modArgs:
 
